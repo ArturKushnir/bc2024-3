06bed7524f6f4219cc84d1cb3bbd8e1a7042b91a
ArturKushnir bc2024-3 <artur.v.kushnirr@gmail.com>
2024-10-28 16:57:21 +0200

    Added main.js

--- main.js
+++ main.js
@@ -1,32 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+const { program } = require('commander');
 
-// Отримуємо аргументи командного рядка
-const args = process.argv.slice(2);
-const params = {};
+program
+  .option('-n, --name <type>', 'Ваше імʼя')
+  .parse(process.argv);
 
-// Розбираємо аргументи командного рядка
-for (let i = 0; i < args.length; i += 2) {
-    const arg = args[i];
-    const value = args[i + 1];
-    if (arg === '-i' || arg === '--input') {
-        params.input = value;
-    } else if (arg === '-o' || arg === '--output') {
-        params.output = value;
-    } else if (arg === '-d' || arg === '--display') {
-        params.display = true;
-        i--;  // Цей параметр не потребує значення
-    }
-}
-
-// Перевірка на обов'язковий параметр
-if (!params.input) {
-    console.error("Please, specify input file");
-    process.exit(1);
-}
-
-// Перевірка на існування файлу
-if (!fs.existsSync(params.input)) {
-    console.error("Cannot find input file");
-    process.exit(1);
-}
+const options = program.opts();
+console.log(`Привіт, ${options.name || 'Світ'}!`);
